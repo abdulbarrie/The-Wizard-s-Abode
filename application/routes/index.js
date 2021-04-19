1edfa5c7025d40f98a4e947eb39d1406c68a6c30
Abdul Barrie <asbarrie14@yahoo.com>
2021-04-18 22:42:57 -0700

    added HTML files from previous assignments intopublic, converted to Handlebars

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,10 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{name:"a god"});
+  res.render('home',{name:"poop"});
 });
 
 router.get('/login',(req, res, next) => {
-  res.render("login");
+  res.render('login');
+})
+
+router.get('/registration',(req, res, next) => {
+  res.render('registration');
+})
+
+router.get('/postimage',(req, res, next) => {
+  res.render('postimage');
+})
+
+router.get('/imagepost',(req, res, next) => {
+  res.render('postimage');
 })
 module.exports = router;
