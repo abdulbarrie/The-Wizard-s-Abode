c32f9e5c7a97313a9cccafc3ffde6c1ca5cedee4
Abdul Barrie <asbarrie14@yahoo.com>
2021-04-19 23:06:24 -0700

    created partials, added nav bar and image borders, and touched up my HTML files.

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,22 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home',{name:"poop"});
+  res.render('home',{title:"HomePage"});
 });
 
 router.get('/login',(req, res, next) => {
-  res.render('login');
+  res.render('login',{title:"Login"});
 })
 
 router.get('/registration',(req, res, next) => {
-  res.render('registration');
+  res.render('registration',{title:"Registration"});
 })
 
 router.get('/postimage',(req, res, next) => {
-  res.render('postimage');
+  res.render('postimage',{title:"PostImage"});
 })
 
 router.get('/imagepost',(req, res, next) => {
-  res.render('postimage');
+  res.render('postimage',{title:"Image Posted"});
 })
 module.exports = router;
