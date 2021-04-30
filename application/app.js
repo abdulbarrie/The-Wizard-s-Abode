00e9680a0d840439d5bdda3b7578ee9cb2ad1267
Abdul Barrie <asbarrie14@yahoo.com>
2021-04-30 13:57:18 -0700

    got register portion of website down

--- application/app.js
+++ application/app.js
@@ -3,10 +3,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var handlebars = require('express-handlebars');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dbRouter = require('./routes/dbtest');
+var errorPrint = require('./helpers/debug/debugprinters').errorPrint;
+var successPrint = require('./helpers/debug/debugprinters').successPrint;
+var requestPrint = require('./helpers/debug/debugprinters').requestPrint;
 
 var app = express();
 
@@ -37,9 +39,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/public", express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    requestPrint(req.url);
+
+
+
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/dbtest', dbRouter)
 app.use('/users', usersRouter);
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.render('error', {err_message: err});
+});
 
 app.use((err, req, res, next) => {
     res.status(500);
--- application/routes/users.js
+++ application/routes/users.js
@@ -1,9 +1,73 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../conf/database');
+var UserError = require('../helpers/error/UserError');
+var errorPrint = require('../helpers/debug/debugprinters').errorPrint;
+var successPrint = require('../helpers/debug/debugprinters').successPrint;
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/register', (req, res, next) => {
+  let username = req.body.uname;
+  let email = req.body.email;
+  let password = req.body.pword;
+  let cpassword = req.body.conpword;
+
+  /**
+   * do server side validation
+   * not done in video must do on your own
+   */
+
+  db.execute("SELECT * FROM users WHERE username=?", [username])
+  .then(([results, fields]) => {
+    if( results && results.length == 0) {
+      return db.execute("SELECT * FROM users WHERE email=?", [email]);
+    } else {
+      throw new UserError (
+        "Registration Failed: Username already exists",
+        "/registration",
+        200
+      );
+    }
+   })
+   .then(([results, fields]) => {
+    if(results && results.length == 0) {
+      let baseSQL = "INSERT into users (username, email, password, created) VALUES (?,?,?,now());"
+      return db.execute(baseSQL,[username, email, password])
+    } else {
+      throw new UserError(
+        "Registration Failed: Email already exists",
+        "/registration",
+        200
+      );
+    }
+   })
+   .then(([results, fields]) => {
+     if(results && results.affectedRows) {
+        successPrint("User.js --> User was created!!");
+        res.redirect('/login');
+     } else {
+       throw new UserError(
+         "Server Error, user could not be created",
+         "/registration",
+         500
+       );
+     }
+   })
+   .catch((err) => {
+    errorPrint("User could not be made", err);
+     if (err instanceof UserError) {
+        errorPrint(err.getMessage());
+        res.status(err.getStatus());
+        res.redirect(err.getRedirectURL());
+     } else {
+       next(err);
+     }
+   });
 });
 
 module.exports = router;
