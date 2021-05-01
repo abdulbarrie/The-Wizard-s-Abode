bdaadd593d6f01e28fff02b0bd7a229ebff1c989
Abdul Barrie <asbarrie14@yahoo.com>
2021-05-01 15:54:10 -0700

    Added Express Middleware; pretty much done with Milestone 3

---
+++ application/middleware/routeprotectors.js
@@ -0,0 +1,16 @@
+const { successPrint, errorPrint } = require("../helpers/debug/debugprinters");
+const routeProtectors = {};
+
+routeProtectors.userIsLoggedIn = function(req, res, next) {
+    if (req.session.username) {
+        successPrint('User is logged in.');
+        next();
+    } else {
+        errorPrint('User is not logged in!');
+        req.flash('error', 'You must be logged in to create a new post!');
+        res.redirect('/login');
+    }
+}
+
+
+module.exports = routeProtectors;
--- application/routes/index.js
+++ application/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var isLoggedIn = require('../middleware/routeprotectors').userIsLoggedIn;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,7 +15,8 @@ router.get('/registration',(req, res, next) => {
   res.render('registration',{title:"Registration - The Wizard's Abode"});
 })
 
-router.get('/postimage',(req, res, next) => {
+router.use('/postimage', isLoggedIn);
+router.get('/postimage', (req, res, next) => {
   res.render('postimage',{title:"Post a New Image - The Wizard's Abode"});
 })
 
