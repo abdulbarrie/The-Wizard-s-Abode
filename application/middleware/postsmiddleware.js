9e1a755f45b78e603d6691b1a19311b1746000ce
Abdul Barrie <asbarrie14@yahoo.com>
2021-05-14 00:25:21 -0700

    As of this point, I have watched all videos and implemented all features. Now all I need to work on is formatting.

--- application/middleware/postsmiddleware.js
+++ application/middleware/postsmiddleware.js
@@ -1,4 +1,5 @@
 const {getNRecentPosts, getPostById} = require('../models/Posts');
+const {getCommentsForPost} = require('../models/comments');
 const postMiddleware = {}
 
 postMiddleware.getRecentPosts = async function(req, res, next) {
@@ -27,6 +28,17 @@ postMiddleware.getPostById = async function(req, res, next) {
         }
     } catch (error) {
         next (err);
+    }
+}
+
+postMiddleware.getCommentsByPostId = async function(req, res, next) {
+    let postId = req.params.id;
+    try {
+        let results = await getCommentsForPost(postId);
+        res.locals.currentPost.comments = results;
+        next();
+    } catch (error) {
+        next(error);
     }
 }
 
--- application/routes/index.js
+++ application/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var isLoggedIn = require('../middleware/routeprotectors').userIsLoggedIn;
-const {getRecentPosts, getPostById} = require("../middleware/postsmiddleware");
+const {getRecentPosts, getPostById, getCommentsByPostId} = require("../middleware/postsmiddleware");
 var db = require('../conf/database');
 
 /* GET home page. */
@@ -26,7 +26,7 @@ router.get('/imagepost',(req, res, next) => {
   res.render('imagepost',{title:"Image of the Day - The Wizard's Abode"});
 });
 
-router.get('/post/:id(\\d+)', getPostById, (req, res, next) => {
+router.get('/post/:id(\\d+)', getPostById, getCommentsByPostId, (req, res, next) => {
     res.render('individualpost', {title: `Post ${req.params.id}`});
 });
 
